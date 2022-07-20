bba78061718674e49657fb685765d17d5423ec8e
Badu Blanc <badu@hey.com>
2022-07-20 18:58:16 -0500

    logic for updateProgressBar

--- src/lib/components/Timer.svelte
+++ src/lib/components/Timer.svelte
@@ -1,15 +1,33 @@
 <script>
 	import { Auction } from '$lib/store';
+	import dayjs from 'dayjs';
 
-	let totalAuctionTime;
+	let now, totalAuctionTime, timeRemaining, progress;
 
 	$: if ($Auction?.startTime && $Auction?.endTime) {
-		totalAuctionTime = $Auction?.endTime.diff($Auction.startTime, 's');
+		totalAuctionTime = $Auction?.endTime.diff($Auction.startTime, 's') || 0;
+		updateProgressBar();
 	}
+
+	const updateProgressBar = () => {
+		now = dayjs();
+
+		// if auction is over, set status & progress bar
+		if (now.isAfter($Auction.endTime)) {
+			console.log('auction over');
+			progress = totalAuctionTime;
+			// also check if settled or not
+			return;
+		}
+
+		timeRemaining = $Auction.endTime.diff(now, 's');
+		progress = totalAuctionTime - timeRemaining;
+		console.log(timeRemaining + 'seconds left in auction');
+	};
 </script>
 
 {#if $Auction}
-	<div>
+	<div class="px-3">
 		<label for="timer" class="flex items-center justify-between">
 			<p>#{$Auction.id}</p>
 			<p>Awaiting Settlement</p>
@@ -17,8 +35,8 @@
 
 		<progress
 			id="timer"
-			value="65"
-			max="100"
+			value={progress}
+			max={totalAuctionTime}
 			class="h-3 w-full rounded-full border-2 border-black"
 		/>
 	</div>
