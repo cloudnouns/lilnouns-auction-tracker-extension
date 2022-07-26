dac37ec176fb2c639af1771072496b2e42c03644
Badu Blanc <badu@hey.com>
2022-07-26 10:01:47 -0500

    hide scrollbar

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -17,7 +17,7 @@
 </script>
 
 <div
-	class="h-[600px] max-h-[600px] w-full max-w-[375px] overflow-x-hidden bg-gradient-to-b from-lime-100 to-cloudnoun-green"
+	class="h-[600px] max-h-[600px] w-full max-w-[375px] overflow-x-hidden bg-gradient-to-b from-lime-100 to-cloudnoun-green scrollbar-hide"
 >
 	<slot />
 </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -15,7 +15,7 @@ const config = {
 		}
 	},
 
-	plugins: []
+	plugins: [require('tailwind-scrollbar-hide')]
 };
 
 module.exports = config;
